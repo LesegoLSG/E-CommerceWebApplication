cfc9b14bf991f79ec08f3bc5d1293e898f83a4e9
LesegoLSG <lesegomhlongo78@gmail.com>
2025-01-15 18:55:07 -0600

    Modify category section

--- E-CommerceWeb/src/Pages/Home/Banner.jsx
+++ E-CommerceWeb/src/Pages/Home/Banner.jsx
@@ -3,9 +3,11 @@ import { slideImages } from "./SlideImages";
 import { RxDotFilled } from "react-icons/rx";
 import { motion } from "framer-motion";
 import { useSwipeable } from "react-swipeable";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Banner = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0); //Current index for a slider
+  const [isCategoryOpen, setIsCategoryOpen] = useState(false); //toggle state
 
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
@@ -55,49 +57,81 @@ const Banner = () => {
   ];
 
   return (
-    <section className=" max-w-screen-2xl min-h-[60vh] md:min-h-[70vh]  container mx-auto flex  xl:px-28 px-4 gap-x-2 mt-4">
+    <section className=" max-w-screen-2xl h-auto md:min-h-[70vh]  container mx-auto flex   xl:px-28 px-4 gap-x-2 mt-4">
       {/* Banner */}
-      <div className="hidden md:block md:w-1/3 h-auto bg-gray-500 rounded shadow-lg">
-        <h2 className="text-lg font-bold mb-4">Shop by Categories</h2>
-        <ul className="space-y-3">
-          {categories.map((category) => (
-            <li
-              key={category.id}
-              className="cursor-pointer hover:bg-gray-200 p-2 rounded-lg"
-              onClick={() => scrollToSection(category.id)}
+      <div className="w-full flex flex-col md:flex-row gap-2">
+        {/* Category Section */}
+        <div className="md:w-1/3 ">
+          {/* Toggle button for small screens */}
+          <div className="md:hidden flex justify-between items-center mb-4">
+            <h2 className="text-lg font-bold">Shop by Category</h2>
+            {/* <button
+              className="text-sm text-white bg-blue-500 px-4 py-1 rounded"
+              onClick={() => setIsCategoryOpen((prev) => !prev)}
             >
-              {category.name}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div
-        className="w-full md:w-full h-auto bg-gray-300  relative"
-        {...handlers}
-      >
-        <motion.div
-          key={currentIndex} // This will trigger animation on index change
-          initial={{ opacity: 0 }} // Animation starts off-screen
-          animate={{ opacity: 1, x: 0 }} // Animates into view
-          exit={{ opacity: 0 }} // Exits off-screen
-          transition={{ duration: 0.8, ease: "easeInOut" }} // Smooth transition
-          style={{ backgroundImage: `url(${slideImages[currentIndex].image})` }}
-          className="w-full h-full bg-center bg-cover rounded duration-600"
-        ></motion.div>
-        {/* slider dots */}
-        <div className="absolute bottom-2 left-1/2 right-1/2  flex justify-center items-center">
-          {slideImages.map((slide, slideIndex) => (
+              {isCategoryOpen ? "Close" : "Open"}
+            </button> */}
             <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className={`text-2xl cursor-pointer ${
-                currentIndex === slideIndex ? "text-white" : "text-gray-500"
-              }`}
+              className=""
+              onClick={() => setIsCategoryOpen((prev) => !prev)}
             >
-              <RxDotFilled />
+              {isCategoryOpen ? (
+                <FaTimes className="w-5 h-5 text-Black" />
+              ) : (
+                <FaBars className="w-5 h-5 text-Black" />
+              )}
             </div>
-          ))}
+          </div>
+
+          {/* Category list */}
+          <div
+            className={`${
+              isCategoryOpen ? "block" : "hidden"
+            } md:block p-4 rounded shadow-lg`}
+          >
+            <ul className="space-y-3">
+              {categories.map((category) => (
+                <li
+                  key={category.id}
+                  className="cursor-pointer hover:bg-Green p-2 rounded-lg text-[#111212] font-semibold"
+                  onClick={() => scrollToSection(category.id)}
+                >
+                  {category.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div
+          className="w-full md:w-full h-auto bg-gray-300  relative"
+          {...handlers}
+        >
+          <motion.div
+            key={currentIndex} // This will trigger animation on index change
+            initial={{ opacity: 0 }} // Animation starts off-screen
+            animate={{ opacity: 1, x: 0 }} // Animates into view
+            exit={{ opacity: 0 }} // Exits off-screen
+            transition={{ duration: 0.8, ease: "easeInOut" }} // Smooth transition
+            style={{
+              backgroundImage: `url(${slideImages[currentIndex].image})`,
+            }}
+            className="w-full h-full min-h-[16rem] bg-center bg-cover rounded duration-600"
+          ></motion.div>
+          {/* slider dots */}
+          <div className="absolute bottom-2 left-1/2 right-1/2  flex justify-center items-center">
+            {slideImages.map((slide, slideIndex) => (
+              <div
+                key={slideIndex}
+                onClick={() => goToSlide(slideIndex)}
+                className={`text-2xl cursor-pointer ${
+                  currentIndex === slideIndex ? "text-white" : "text-gray-500"
+                }`}
+              >
+                <RxDotFilled />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
