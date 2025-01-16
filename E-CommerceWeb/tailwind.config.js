ef4b8f84934de4c4d35d30c9eb8e0a587774fe1c
LesegoLSG <lesegomhlongo78@gmail.com>
2025-01-15 19:53:26 -0600

    Add sky blue color

--- E-CommerceWeb/tailwind.config.js
+++ E-CommerceWeb/tailwind.config.js
@@ -10,7 +10,8 @@ export default {
         "Black": "#111212",
         "primaryBG":"#f3f2f7",
         "Purple":"#871ff0",
-        "Green":"#5fd957"
+        "Green":"#5fd957",
+        "SkyBlue":"#57A3D9"
       },
       fontFamily:{
         "primary":['Roboto', 'sans-serif'],
